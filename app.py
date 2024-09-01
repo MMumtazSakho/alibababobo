5c33993df23a85654be61612bec79b50fcb1304d
Fadil Fauzan <128146609+fadilmf@users.noreply.github.com>
2024-09-01 08:33:57 +0700

    Update app.py host

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ def generate(chapter_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, port=5000)
+    # app.run(debug=True, use_reloader=False, port=5000)
+    app.run(host='0.0.0.0', port=5000)
